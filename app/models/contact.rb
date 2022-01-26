4ceb9d4557b877d7403270919506fdcf4294ea37
Dhivas-Sugumar <dhivassugumar@gmail.com>
2022-01-26 00:51:16 -0500

    Added contact us page.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form Inquiry",
-      :to => "your.email@address.com",
+      :to => "sdkldhivas@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_021713) do
+ActiveRecord::Schema.define(version: 2022_01_25_052301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,11 @@ ActiveRecord::Schema.define(version: 2022_01_23_021713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "banner"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "courses", force: :cascade do |t|
