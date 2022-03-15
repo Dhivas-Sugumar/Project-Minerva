e111f14a8039576a2c6825c376431d9e0fe87606
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-03-14 17:41:02 -0700

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  resources :enrolls, only: [:show,:destroy,:new,:create,:index]
+  resources :enrolls, only: [:show,:destroy,:new,:create, :index]
   devise_for :users
   resources :contacts, only: [:new, :create]
-  resources :reviews
-  resources :categories
-  resources :videos
-  resources :lessons
-  resources :sections
+  resources :reviews, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :categories, only:[:index, :new, :create, :show, :destroy]
+  resources :videos, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :lessons, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :sections, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :courses
-  resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 
 
   get '/account', to: 'users#account'
