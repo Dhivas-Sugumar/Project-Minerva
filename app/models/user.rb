436b926c964172d11139e6b08ebc2634171dd45c
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-05-05 21:18:40 -0700

    Fixed deactivate account.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :reviews
-  has_many :enrolls
-  has_many :courses
+  has_many :enrolls, dependent: :destroy
+  has_many :courses ,dependent: :destroy
   validates :username, presence: true, length: { minimum: 1, maximum: 50 }
   mount_uploader :avatar, AvatarUploader
 
