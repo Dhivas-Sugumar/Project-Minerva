e4903e4e62f96cbb32e620fc4f3c39c740692812
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-05-05 20:59:17 -0700

    Fixed application.js bug.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,11 @@
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require cocoon
 import "@hotwired/turbo-rails"
-//= require  "controllers"
-//= require "trix"
-//= require  "@rails/actiontext"
-//= require jquery
-//= require jquery-ui
-//= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-
-
+import "controllers"
+import "trix"
+import "@rails/actiontext"
 
 
 
