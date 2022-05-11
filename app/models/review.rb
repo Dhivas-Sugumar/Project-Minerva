93447e1bce23be4a2997e4070ea9f14d221a7036
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-05-11 00:06:06 -0700

    Added user_id to reviews and added get methods to user model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,12 @@ class Review < ApplicationRecord
   belongs_to :course
   belongs_to :user
   has_rich_text :comment
+
+  def get_user
+    User.find(user.id)
+  end
+
+  def get_course
+    Course.find(course.id)
+  end
 end
