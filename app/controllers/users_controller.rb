c72d3240401f0e20d8d60f7ee30eed98892e0f42
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-07-10 18:13:42 -0700

    Fixed user flashes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       format.html { redirect_to user_path(@user), notice: "User was successfully updated." }
-      format.json { render :show, status: :ok, location: @enroll }
+      format.json { render :show, status: :ok, location: @user }
     else
       format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @enroll.errors, status: :unprocessable_entity }
+      flash[:alert] = 'Could not create user. Please try again.'
     end
 
   end
