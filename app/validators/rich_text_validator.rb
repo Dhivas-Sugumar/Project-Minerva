321b1dc342d22000c311ba7ff37d91b473723b7a
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-07-28 05:26:03 -0700

    Changed richtext validator for course chacters to a lower requirement.

--- app/validators/rich_text_validator.rb
+++ app/validators/rich_text_validator.rb
@@ -2,14 +2,14 @@ class RichTextValidator < ActiveModel::Validator
 
   def validate(record)
     if record.has_attribute?(:body)
-    unless record.body.to_plain_text.length > 400
-      record.errors.add :body, "Body can not be less than 250 characters"
+    unless record.body.to_plain_text.length > 250
+      record.errors.add :body, "Body can not be less than 400 characters"
 
     end
     end
     if record.has_attribute?(:description)
-      unless record.description.to_plain_text.length > 250
-      record.errors.add :description, "Description can not be less than 250 characters"
+      unless record.description.to_plain_text.length > 200
+      record.errors.add :description, "Description can not be less than 200 characters"
 
       end
     end
