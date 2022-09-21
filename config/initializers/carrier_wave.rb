c3d497e5606f3b2f24dff1ab68341573396fc0d4
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-09-20 20:42:22 -0700

    Added cloud storage support.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
 
-  if Rails.env.development? || Rails.env.test?
+  if Rails.env.development?  || Rails.env.test?
     CarrierWave.configure do |config|
       config.storage = :file
     end
@@ -16,10 +16,9 @@ CarrierWave.configure do |config|
 
   config.fog_provider = 'fog/google'
   config.fog_credentials = {
-
-    :provider                         => 'Google',
-    :google_storage_access_key_id     => ENV["GOOGLE_STORAGE_ACCESS_KEY_ID"],
-    :google_storage_secret_access_key => ENV["GOOGLE_STORAGE_SECRET_ACCESS_KEY"],
+    provider: 'Google',
+    google_storage_access_key_id: ENV["GOOGLE_STORAGE_ACCESS_KEY_ID"],
+    google_storage_secret_access_key: ENV["GOOGLE_STORAGE_SECRET_ACCESS_KEY"],
 
   }
 
