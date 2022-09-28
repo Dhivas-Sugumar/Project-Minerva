69d17a405166be92280bf74155bb4110f41c5d82
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-09-27 20:20:58 -0700

    Fixed imports on application js file.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-require("jquery")
-require("@nathanvda/cocoon")
-import "cocoon";
 import "@fortawesome/fontawesome-free/css/all.css";
 //= require jquery
+import "cocoon";
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
