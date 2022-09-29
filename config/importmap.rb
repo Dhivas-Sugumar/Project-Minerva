0697da4e1ee949b304697e2edac9e028c02f3ba9
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-09-28 21:13:34 -0700

    FIxed cocoon errors.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
