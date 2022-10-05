46ac22916f040eba16b41d2a7577cebcb850380d
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-04 18:35:55 -0700

    Switched all node packages to cs and js bundling.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "bootstrap";
-import "jquery3"
-import "popper"
+import "jquery";
+import "@popperjs/core/dist/umd/popper";
 import "@hotwired/turbo-rails";
-import "./controllers/index.js"
+import "@nathanvda/cocoon";
 import "@fortawesome/fontawesome-free";
-import "./review"
-
-import * as bootstrap from "bootstrap"
+import "./review";
+import "@splidejs/splide/dist/js/splide.min";
