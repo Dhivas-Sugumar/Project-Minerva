8a4a32ced9382b14b3ae07f73d34d9766b9d7e13
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-04 17:29:14 -0700

    Removed the sprockets application js file.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require bootstrap
+import "bootstrap";
+import "jquery3"
+import "popper"
 import "@hotwired/turbo-rails";
 import "./controllers/index.js"
 import "@fortawesome/fontawesome-free";
-//= require turbolinks
-//= require_tree .
+import "./review"
 
