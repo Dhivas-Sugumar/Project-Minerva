2348b9c7f914796189315dab627872d014647769
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-11 07:28:30 -0700

    css bundling works. added esbuild config file.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "bootstrap";
-import "jquery";
-import "@popperjs/core/dist/umd/popper";
 import "@hotwired/turbo-rails";
 import "@nathanvda/cocoon";
 import "@fortawesome/fontawesome-free";
@@ -10,3 +8,4 @@ import "tinymce/tinymce.min";
 import "./custom/review";
 import "./custom/splide-config";
 import "./custom/tinymce-config";
+console.log("Hello application js esbuild.")
