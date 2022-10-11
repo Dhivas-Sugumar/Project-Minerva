4911e6998e14bcdbcaab82762e18d2e5b3979208
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-11 07:57:30 -0700

    REmoved importmaps from js entirely.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "bootstrap";
 import "@hotwired/turbo-rails";
+import "./controllers"
+
 import "@nathanvda/cocoon";
 import "@fortawesome/fontawesome-free";
 import "@splidejs/splide/dist/js/splide.min";
@@ -8,4 +9,6 @@ import "tinymce/tinymce.min";
 import "./custom/review";
 import "./custom/splide-config";
 import "./custom/tinymce-config";
+import * as bootstrap from "bootstrap"
+
 console.log("Hello application js esbuild.")
