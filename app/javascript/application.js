dc982a4e45af3fcd8be5721326e37e9b56b861f4
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-11 15:42:43 -0700

    SWitched imports around in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './custom/jquery-config'
+import "./custom/tinymce-config";
+import "./custom/review";
+import "./custom/splide-config";
 import "@hotwired/turbo-rails";
 import "@nathanvda/cocoon";
 import "@splidejs/splide/dist/js/splide.min";
 import "tinymce/tinymce.min";
-import "./custom/review";
-import "./custom/splide-config";
-import "./custom/tinymce-config";
+
 import * as bootstrap from "bootstrap"
 
 console.log("Hello application js esbuild.")
