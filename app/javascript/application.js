fbc21ea2e658832d066cde3ebb54c3aff223f815
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-10-13 14:16:45 -0700

    Added missing font awesome files.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './vendor/jquery-config'
 import "@hotwired/turbo-rails";
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import "@nathanvda/cocoon";
 import "@splidejs/splide/dist/js/splide.min";
 import "tinymce";
